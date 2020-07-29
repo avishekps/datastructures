d3672e33789128792f4408bfe432a841fe2d5f6a
avisheks <avishekpratapsingh@gmail.com>
2020-07-29 22:00:53 +0530

    Majority Element problem solution

---
+++ src/com/ds/arrays/MajorityElement.java
@@ -0,0 +1,27 @@
+package com.ds.arrays;
+
+public class MajorityElement {
+
+    public static void main(String[] s) {
+        int[] arr = {2,2,1,1,1,2,2};
+        System.out.println("Majority element of array is: " + majorityElement(arr));
+    }
+
+    public static int majorityElement(int[] nums) {
+
+        int ele = nums[0];
+        int count=1;
+        for (int i=1;i<nums.length;i++) {
+            if (ele == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                ele = nums[i];
+                count++;
+            }
+        }
+        return ele;
+    }
+}
